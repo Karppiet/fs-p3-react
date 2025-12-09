51b8dccc7200c77e093ab3e6305a51c2a84a4c52
karppi <karppi@min.com>
2025-12-09 11:30:17 +0200

    snippetform post added

--- src/components/SnippetForm.jsx
+++ src/components/SnippetForm.jsx
@@ -1,31 +1,60 @@
-import { useState } from "react";
 import axios from "axios";
 // import { useApi } from "../hooks/use_Api";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import React, { useState } from 'react';
 
 const API_BASE_URL =
      import.meta.env.VITE_API_URL;
 
 function SnippetForm() {
 
+  const [title, setTitle] = useState('');
+  const [language, setLanguage] = useState('');
+  const [code, setCode] = useState('');
+
+    const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = { title, language, code };
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/snippets`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
-    <Form  >
+    <div>
+    <Form onSubmit={handleSubmit} >
       <Row>
         <Col>
-          <Form.Control type="title" placeholder="Title"  />
+          <Form.Control type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}  />
         </Col>
         <Col>
-          <Form.Control type="language" placeholder="Language"></Form.Control>
+          <Form.Control type="text" placeholder="Language" value={language} onChange={(e) => setLanguage(e.target.value)} />
         </Col>
         <Col>
-          <Form.Control type="code" placeholder="Code" />
+          <Form.Control type="text" placeholder="Code" value={code}  onChange={(e) => setCode(e.target.value)} />
         </Col>
         <Col>
-          <Form.Control type="tags" placeholder="Tags" />
+          <Form.Control type="texts" placeholder="Tags" />
         </Col>
       </Row>
       <br />
@@ -37,6 +66,7 @@ function SnippetForm() {
         Post
       </button>
     </Form>
+    </div> 
   );
 }
 
