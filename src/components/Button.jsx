ff0268a2837851baf8624adb854f7d85fc2141d9
karppi <karppi@min.com>
2025-12-10 10:59:21 +0200

    No used components deleted

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ children, onClick, type = "button" }) => {
-  return (
-    <button type={type} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
-
-export default Button;
--- src/components/Message.jsx
+++
@@ -1,8 +0,0 @@
-import Form from "react-bootstrap/Form";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
-
-const API_BASE_URL =
-     import.meta.env.VITE_API_URL;
